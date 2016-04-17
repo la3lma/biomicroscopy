cad92cbfcbf6b2d95ce38965b0a70ee7e987c035
Bjørn Remseth <rmz@comoyo.com>
2016-04-17 19:44:12 +0200

    fixer upper

--- src/fetch-and-process-images.sh
+++ src/fetch-and-process-images.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+[! -d images ] && mkdir images
+
 # First fetch all the images
-scp 'pi@10.0.0.23:/home/pi/yeast/images/*'
+scp 'pi@10.0.0.23:/home/pi/yeast/images/*' images
 
 # Then make links with a nameschema that can be
 # picked up by ffmpeg
+[! -d frames ] && mkdir frames
+rm frames/*
+
 FOO=1;
 for x in images /; do
     ln -s ../$x $(printf " frames/%06d . jpg " $FOO) ;
